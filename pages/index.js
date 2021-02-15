891c43df4de311b37850af395bf311dec0ed6ae5
ruth oliveira <ruthholli_@hotmail.com>
2021-02-15 17:00:08 -0300

    link do teste 2

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,14 @@ import Link from "next/link";
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+          <h1>Home</h1>
+          <h2>teste</h2>
       <Link href="/sobre">
         <a>Acessar página Sobre</a>
       </Link>
     </div>
-  );
+    );
+    
 }
 
 export default Home;
